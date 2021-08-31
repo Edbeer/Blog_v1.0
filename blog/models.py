03fa61b8b63570690038986e4865547c807c717a
Edbeer <edbeermtn@gmail.com>
2021-08-31 13:44:28 +0700

    migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.CharField(max_length=150, unique=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.CharField(max_length=150, unique=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.CharField(max_length=150, unique=True, verbose_name='URL')
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    is_published = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+
