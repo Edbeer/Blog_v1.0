bac46cad820457e9b5c8870c3f6c770d9d39728c
Edbeer <edbeermtn@gmail.com>
2021-09-01 14:26:06 +0700

    add register

--- authorization/views.py
+++ authorization/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import *
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'GOOD!')
+            return redirect('home')
+        else:
+            messages.error(request, 'BAD!')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'authorization/register.html', {'form': form})
+
+
+def user_login(request):
+    pass
+
+
+def user_logout(request):
+    pass
