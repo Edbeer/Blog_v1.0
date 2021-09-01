ad11cd41a7d43c40aebc2290479f6a92aa208f02
Edbeer <edbeermtn@gmail.com>
2021-09-01 15:28:33 +0700

    add popular posts

--- blog/templatetags/popular.py
+++ blog/templatetags/popular.py
@@ -4,4 +4,7 @@ from blog.models import Post
 register = template.Library()
 
 
-@register.inclusion_tag()
+@register.inclusion_tag('blog/popular.html')
+def show_popular(cnt=3):
+    posts = Post.objects.order_by('-views')[:cnt]
+    return {'posts': posts}
